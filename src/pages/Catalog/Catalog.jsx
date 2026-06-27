127af8847ab19b421a3133f63ae7ee2558aa1883
BtoMJ <bto.mena.17@gmail.com>
2026-06-27 15:24:37 -0600

    Corrección en íconos del catálogo

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,27 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useCartStore } from "../../store/cartStore.js";
 import CartIcon from "../../components/CardIcon/CardIcon";
-import { FaHouse, FaCartShopping } from "react-icons/fa6";
 import products from "../../data/products.json";
 import ProductCard from "../../components/ProductCard/ProductCard.jsx";
+import { FaHouse, FaCartShopping } from "react-icons/fa6";
 import "./Catalog.css";
 
 function Catalog() {
+  const location = useLocation();
+  console.log(location.pathname);
+
   const items = useCartStore((state) => state.items);
   return (
     <section className="cat-container" id="cat">
       <div className="cat-nav">
-        <div className="icon-cat">
+        <div className="icon-cat-container">
           <NavLink to="/">
-            <FaHouse />
+            <FaHouse className="icon-cat" />
           </NavLink>
         </div>
         <h2>Productos</h2>
-        <div className="icon-cat">
-          <NavLink to="/cart" className="nav-cart-mobile">
-            <CartIcon />
+        <div className="icon-cat-container">
+          <NavLink to="/cart">
+            <CartIcon className="cart-icon-cat" />
             <FaCartShopping
-              className={items.length > 0 ? "items-cart" : "no-items-cart"}
+              className={items.length > 0 ? "icon-cat" : "icon-cat-cart"}
             />
           </NavLink>
         </div>
